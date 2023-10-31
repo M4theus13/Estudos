5791d8b8e25e8e80e2688bb37a9c0a3bb1b734b7
MatheusT <matheusgarciajbs@gmail.com>
2023-10-31 12:17:53 -0300

    webcam

---
+++ JavaScript/Camera/script.js
@@ -0,0 +1,28 @@
+function startVideoFromCamera() {
+
+    const specs = {video:{width:320}}
+
+    navigator.mediaDevices.getUserMedia(specs).then(stream => {
+        const videoElement = document.getElementById('video')
+        videoElement.srcObject = stream
+        
+    }).catch(error=>{console.log(error)})
+}
+
+window.addEventListener('DOMContentLoaded', startVideoFromCamera)
+const button = document.getElementById('capture')
+button.addEventListener('click', ()=> {
+    const canvas = document.getElementById('foto')
+    canvas.height = video.height
+    canvas.width = video.width
+
+
+    const context = canvas.getContext('2d')
+    canvas.drawImage(video, 0,0)
+
+    const link = document.createElement('a')
+    link.download = 'foto.png'
+    link.href = canvas.toData()
+    link.textContent = 'Clique para baixar a imagem'
+    document.body.appendChild(link)
+})
